72642e23c94cab79a7c35cf763451a9fb5c9d48a
KATfreal <122062450+KATfreal@users.noreply.github.com>
2023-11-16 15:38:12 -0400

    Add files via upload

--- test.java
+++ test.java
@@ -1,36 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class test {
+	static ArrayList<Library> liblist = new ArrayList<Library>();
+	static Library placeholder = new Library("place", "1holder");
     public static void main(String[] args) {
 
-
-        Library lib1 = new Library("library1", "123 Street");
-        Library lib2 = new Library("library2", "124 Street");
-        Library lib3 = new Library("library3", "125 Street");
-        Library lib4 = new Library("library4", "126 Street");
-        Library lib5 = new Library("library5", "127 Street");
-        Library lib6 = new Library("library6", "128 Street");
-        Library lib7 = new Library("library7", "129 Street");
-        Library lib8 = new Library("library8", "130 Street");
-        Library lib9 = new Library("library9", "131 Street");
-        Library lib10 = new Library("library10", "132 Street");
+    	Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter library quantity:");
+        String number = myObj.nextLine();  // Read user input
+        int libquant = Integer.valueOf(number);
+        
+       
+        LibraryManagementSystem l1 = new LibraryManagementSystem(placeholder);
+        
+        
+        libadd(libquant);
+        lbmsadd(l1);
+        
         
         Member m1 = new Member(2000, "First", "Last", "Male");
         Member m2 = new Member(2001, "First", "Last", "Female");
 
-        LibraryManagementSystem l1 = new LibraryManagementSystem(lib1);
-
-        l1.addLibrary(lib1);
-        l1.addLibrary(lib2);
-        l1.addLibrary(lib3);
-        l1.addLibrary(lib4);
-        l1.addLibrary(lib5);
-        l1.addLibrary(lib6);
-        l1.addLibrary(lib7);
-        l1.addLibrary(lib8);
-        l1.addLibrary(lib10);
-        l1.addLibrary(lib9);
 
         l1.addMember(m1);
         l1.addMember(m2);
    
+    }
+    private static void libadd(int quant) 
+    {
+    	for(int i = 0; i<quant;i++) {
+    	Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    	int num = i+1;
+        System.out.println("Enter library name"+" "+num+":");
+
+        String userName = myObj.nextLine();  // Read user input
+        
+        Scanner myObj2 = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter street name"+" "+num+":");
+
+        String useraddy = myObj.nextLine();  // Read user input
+        Library lib1 = new Library(userName, useraddy);
+        liblist.add(lib1);
+        //System.out.println("Username is: " + userName);  // Output user input
+        
+    	
+    	}
+    	
+    	
+    }
+    static void lbmsadd(LibraryManagementSystem l1) 
+    {
+    	//LibraryManagementSystem l1 = new LibraryManagementSystem(placeholder);
+    	for(int i = 0; i<liblist.size();i++) {
+    		l1.addLibrary(liblist.get(i));
+    	}
+    	
+    	
+    	
     }
 }
