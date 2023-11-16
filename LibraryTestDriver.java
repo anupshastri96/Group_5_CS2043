54c7d0f7863fa22f25f1d68260715203c29f9a4c
Peyton Hooper <phooper@unb.ca>
2023-11-16 13:14:11 -0400

    Added 'toString method to book; Updated LibraryTestDriver to use new toString

--- LibraryTestDriver.java
+++ LibraryTestDriver.java
@@ -3,24 +3,38 @@ public class LibraryTestDriver{
 	public static void main(String args[]) {
 	
 		Book b1 = new Book("The Hunger Games", "Suzanne Collins",  800, false);
-	
-		System.out.println(b1.getName());
-		System.out.println(b1.getAuthor());
-		System.out.println(b1.getId());
-		System.out.println(b1.getDewey());
-		System.out.println(b1.getAmount());
-		System.out.println(b1.getAdult());
 		
-		for(int i=0; i<b1.getLibraryIDs().length; i++) {
-			System.out.println(b1.getLibraryIDs()[i]);
+		/*
+		public String toString() {
+			String s =	"Title: " + getName() + "\n" +
+						"Author: " + getAuthor() + "\n" +
+						"Dewey: " + getDewey() + "\n" +
+						"Book ID: " + getId() + "\n" +
+						"Amount: " + getAmount() + "\n" +
+						"Amount Borrowed: " + amountBorrowed + "\n" +
+						"LibraryIDs: ";
+		
+			if (getLibraryIDs().length == 0) s += "No LibraryIDs\n";
+			else {
+				for(int i=0; i<getLibraryIDs().length; i++) {
+					s += getLibraryIDs()[i] + "\n";
+				}
+			}
+		
+			s += "MemberIDs: ";
+		
+			if (getMemberIDs().length == 0) s += "No MemberIDs";
+			else {
+				for(int j=0; j<getMemberIDs().length; j++) {
+					s += getMemberIDs()[j] + "\n";
+				}
+			}
+		
+			return s;
 		}
-		if (b1.getLibraryIDs().length == 0) System.out.println("No LibraryIDs");
+		*/
 		
-		for(int j=0; j<b1.getMemberIDs().length; j++) {
-			System.out.println(b1.getMemberIDs()[j]);
-		}
-		if (b1.getMemberIDs().length == 0) System.out.println("No MemberIDs");
-		
+		System.out.print(b1.toString());
 		
 		//Library l1 = new Library("Library 1", "123 Book Rd");
 	}
