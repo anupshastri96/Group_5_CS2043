e98ea267f16eb0bd20e743636f131829659cbc31
SamuelConrad <c7bbm@unb.ca>
2023-11-29 14:14:52 -0400

    Made changes to libraryedit GUI

--- GUILibraryEdit.java
+++ GUILibraryEdit.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
@@ -17,6 +18,7 @@ public class GUILibraryEdit extends JFrame implements ActionListener {
 
     JTextField nameTextField;
     JTextField addressTextField;
+    JPanel contentPane;
     JButton submitButton;
     JButton backButton;
     Library storeLibrary;
@@ -25,53 +27,68 @@ public class GUILibraryEdit extends JFrame implements ActionListener {
 
         storeLibrary = libraryIn;
 
-        submitButton = new JButton("Submit Changes");
-        submitButton.setBounds(100,100,100,40);
-        submitButton.setFocusable(false);
-        submitButton.addActionListener(this);
+        this.setTitle("Current Library: " + LibraryManagementSystem.getCurrentLibrary().getName());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(375, 340);
+        this.setResizable(false);
+
+        contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
         backButton = new JButton("Back");
-		backButton.setBounds(100,100,100,40);
+		backButton.setBounds(10, 15, 68, 50);
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        submitButton = new JButton("Submit");
+        submitButton.setBounds(140,240,85,50);
+        submitButton.setFocusable(false);
+        submitButton.addActionListener(this);
+
+        JLabel nameLabel = new JLabel("Change name:");
+        nameLabel.setBounds(10, 100, 200, 35);
+
         nameTextField = new JTextField();
-        nameTextField.setPreferredSize(new Dimension(250,40));
+        nameTextField.setBounds(130, 100, 200, 35);
+
+        JLabel addressLabel = new JLabel("Change address:");
+        addressLabel.setBounds(10, 150, 200, 35);
 
         addressTextField = new JTextField();
-        addressTextField.setPreferredSize(new Dimension(250,40));
+        addressTextField.setBounds(130, 150, 200, 35);
 
+        Font buttonFont = new Font("Arial", Font.PLAIN, 16);
+        Font titleFont = new Font("Arial", Font.BOLD, 18);
 
-        this.setTitle("Current Library: " + LibraryManagementSystem.getCurrentLibrary().getName());
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(300, 350);
-        this.setResizable(false);
-        this.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+        backButton.setFont(buttonFont);
+        nameLabel.setFont(buttonFont);
+        nameTextField.setFont(buttonFont);
+        addressLabel.setFont(buttonFont);
+        addressTextField.setFont(buttonFont);
+        submitButton.setFont(buttonFont);
 
-        JPanel topPanel = new JPanel();
-        topPanel.setPreferredSize(new Dimension(300,40));
-        topPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
-
-        JPanel panel2 = new JPanel();
-        panel2.setPreferredSize(new Dimension(300,300));
-        panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-        topPanel.add(backButton);
-        this.add(topPanel);
-
-        panel2.add(nameTextField);
-        panel2.add(addressTextField);
-        panel2.add(submitButton);
-        this.add(panel2);
-
+        contentPane.add(nameLabel);
+        contentPane.add(nameTextField);
+        contentPane.add(addressLabel);
+        contentPane.add(addressTextField);
+        contentPane.add(submitButton);
+        contentPane.add(backButton);
+        
         this.setVisible(true);
+        
     }
 
     public void actionPerformed(ActionEvent e) {
         
         if (e.getSource() == submitButton) {
-            storeLibrary.changeName(nameTextField.getText());
-            storeLibrary.changeAddress(addressTextField.getText());
+            if (!nameTextField.getText().equals("")) {
+                storeLibrary.changeName(nameTextField.getText());
+            } 
+            if (!addressTextField.getText().equals("")) {
+                storeLibrary.changeAddress(addressTextField.getText());
+            }
             LibraryManagementSystem.libraryWriteFile();
             this.dispose();
             GUILibraryShow show = new GUILibraryShow(storeLibrary);
