2a47b87529d5fcf1a08169d5ad53f8f8b0fe89b4
SamuelConrad <c7bbm@unb.ca>
2023-11-29 15:33:26 -0400

    redid memberadd GUI

--- GUIMemberAdd.java
+++ GUIMemberAdd.java
@@ -7,9 +7,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,97 +21,112 @@ import java.util.Random;
 public class GUIMemberAdd extends JFrame implements ActionListener{
 
 	JPanel contentPane;
-	JTextField Nametxt;
-	JTextField lasttxt;
-	JTextField DOBtxt;
-	JTextField addtxt;
+	JTextField fnText;
+	JTextField lnText;
+	JTextField YOBText;
+	JTextField addressText;
 	JComboBox<String> genderField;
 	JButton add;
 	JButton backButton;
 	String[] genders = {"Male", "Female", "Other", "Prefer to not say"};
 
-	/**
-	 * Create the frame.
-	 */
 	public GUIMemberAdd() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 450);
-		contentPane = new JPanel();
+		this.setTitle("Current Library: " + LibraryManagementSystem.getCurrentLibrary().getName());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(375, 340);
+        this.setResizable(false);
+
+        Font font = new Font("Arial", Font.PLAIN, 16); 
+
+        contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("MEMBERSHIP");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		lblNewLabel.setBounds(287, 11, 111, 36);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("FIRST NAME:");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblNewLabel_1.setBounds(129, 107, 81, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("LAST NAME:");
-		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblNewLabel_1_1.setBounds(129, 146, 81, 14);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JLabel lblNewLabel_1_2 = new JLabel("DATE OF BIRTH:");
-		lblNewLabel_1_2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblNewLabel_1_2.setBounds(129, 188, 103, 14);
-		contentPane.add(lblNewLabel_1_2);
-		
-		JLabel lblNewLabel_1_3 = new JLabel("ADDRESS:");
-		lblNewLabel_1_3.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblNewLabel_1_3.setBounds(129, 236, 81, 14);
-		contentPane.add(lblNewLabel_1_3);
-		
-		JLabel lblNewLabel_1_4 = new JLabel("GENDER:");
-		lblNewLabel_1_4.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblNewLabel_1_4.setBounds(129, 270, 81, 14);
-		contentPane.add(lblNewLabel_1_4);
-		
-		Nametxt = new JTextField();
-		Nametxt.setBounds(280, 104, 146, 20);
-		contentPane.add(Nametxt);
-		Nametxt.setColumns(10);
-		
-		lasttxt = new JTextField();
-		lasttxt.setColumns(10);
-		lasttxt.setBounds(280, 143, 146, 20);
-		contentPane.add(lasttxt);
-		
-		DOBtxt = new JTextField();
-		DOBtxt.setColumns(10);
-		DOBtxt.setBounds(280, 185, 146, 20);
-		contentPane.add(DOBtxt);
-		
-		addtxt = new JTextField();
-		addtxt.setColumns(10);
-		addtxt.setBounds(280, 233, 146, 20);
-		contentPane.add(addtxt);
-		
-		genderField = new JComboBox<>(genders);
-		genderField.setBounds(280, 275, 146, 20);
-		contentPane.add(genderField);
-		
-		add = new JButton("ADD MEMBER");
-		add.addActionListener(this);
-		add.setBounds(309, 315, 111, 23);
-		contentPane.add(add);
 
-		this.setVisible(true);
+        backButton = new JButton("Back");
+		backButton.setBounds(10, 15, 68, 50);
+        backButton.setFocusable(false);
+        backButton.addActionListener(this);
+        backButton.setFont(font);
+
+        contentPane.add(backButton);
+
+        JLabel nameLabel = new JLabel("Firstname:");
+        nameLabel.setBounds(10, 75, 80, 25);
+        nameLabel.setFont(font);
+        contentPane.add(nameLabel);
+
+        fnText = new JTextField(20);
+        fnText.setBounds(115, 75, 220, 25);
+        contentPane.add(fnText);
+
+        JLabel lastLabel = new JLabel("Lastname:");
+        lastLabel.setBounds(10, 115, 80, 25);
+        lastLabel.setFont(font);
+        contentPane.add(lastLabel);
+
+        lnText = new JTextField(20);
+        lnText.setBounds(115, 115, 220, 25);
+        lnText.setFont(font);
+        contentPane.add(lnText);
+
+        JLabel addressLabel = new JLabel("Address:");
+        addressLabel.setBounds(10, 155, 110, 25);
+        addressLabel.setFont(font);
+        contentPane.add(addressLabel);
+
+		addressText = new JTextField(20);
+        addressText.setBounds(115, 155, 220, 25);
+        addressText.setFont(font);
+        contentPane.add(addressText);
+
+		JLabel genderLabel = new JLabel("Gender:");
+        genderLabel.setBounds(10, 195, 110, 25);
+    	genderLabel.setFont(font);
+        contentPane.add(genderLabel);
+
+        genderField = new JComboBox<>(genders);
+        genderField.setBounds(115, 195, 220, 25);
+        genderField.setFont(font);
+        contentPane.add(genderField);
+
+		JLabel yobLabel = new JLabel("Year of birth:");
+        yobLabel.setBounds(10, 235, 110, 25);
+    	yobLabel.setFont(font);
+        contentPane.add(yobLabel);
+
+        YOBText = new JTextField(20);
+        YOBText.setBounds(115, 235, 220, 25);
+        YOBText.setFont(font);
+        contentPane.add(YOBText);
+
+        add = new JButton("Add Member");
+        add.setBounds(200, 15, 150, 50);
+        contentPane.add(add);
+        add.setFont(font);
+        add.addActionListener(this);
+
+        this.setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent e) {
         if (e.getSource() == add) {
-            String gender = (String) genderField.getSelectedItem();
-			String address = addtxt.getText();
-			String DOB = DOBtxt.getText();
-			String Fname = Nametxt.getText();
-			String Lname = lasttxt.getText();
-			Member membernew = new Member(Integer.parseInt(DOB),Fname,Lname,gender,address);
-			LibraryManagementSystem.addMember(membernew);
+			try {
+				String gender = "";
+				if (((String) genderField.getSelectedItem()).equals("Prefer not to say")) {
+					gender = "None";
+				} else {
+					gender = (String) genderField.getSelectedItem();
+				}
+				String address = addressText.getText();
+				String YOB = YOBText.getText();
+				String Fname = fnText.getText();
+				String Lname = lnText.getText();
+				Member membernew = new Member(Integer.parseInt(YOB),Fname,Lname,gender,address);
+				LibraryManagementSystem.addMember(membernew);
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(null, "Error occured while parsing year of birth");
+			}
         }  else if (e.getSource() == backButton) {
             this.dispose();
             GUIMemberAdd add = new GUIMemberAdd();
